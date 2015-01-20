a840a4652b205f7121d5c58437b3fe0277f00acf
DezerteR <wajskarol@gmail.com>
2015-01-20 19:50:53 +0100

    Update

--- javascripts/Kulki.js
+++ javascripts/Kulki.js
@@ -3,6 +3,7 @@
 	//set colors with random values
 	// var color = iio.randomColor();
 	var color = 'gray';
+	var colors = ["red","green", "blue", "yellow", "gray", "black", "aqua", "fuchsia", "lime", "navy" ];
 	var inverted = iio.invertColor(color);
 	app.set(inverted);
 
@@ -10,7 +11,8 @@
 	var res = 10;
 	var ballLevel = 9;
 	var ballCount = 0;
-
+	var points = 0;
+	
 	//create the grid
 	var grid = app.add(app.center,
 		(( app.height < app.width ) ? app.height-50 : app.width-50)
@@ -87,35 +89,38 @@
 		var resolvePatch = function(){
 			var path = new Array(0);
 			var s = to;
-			while(from.x!=to.x || from.y!=to.y){
+			var target = from;
+			path.push(s);
+			var count = 0;
+			set_g(from, 0);
+			var minval = 1000;
+			while(minval!=0 && count++<200 ){
 				var it;
 				var n;
-				var minval = 100000;
+				minval = 100000;
 				for(n of neighbours){
 					var u = Add(s,n);
 					var uval = g(u);
-					console.log(uval);
-					if(uval < minval && graph[u.x+1][u.y+1]!=-2 && uval>-1){
+					if(uval < minval && (graph[u.x+1][u.y+1]!=-2 || uval ==0) && uval>-1){
 						minval = uval;
 						it = u;
-						
 					}
 				}
-				if(it.x){
-					s = Add(s, it);
-					set_g(s, 10000);
+				if(it != 'undefined'){
+					// s = Add(s, it);
+					s = it;
+					set_g(s, -10);
 					path.push(s);
 				}
 				else 
-					break;
-				
-				
+					return false;
+			
 			}
 			return path;
 		}
 		
-		set_g(from, 0);
 		
+		set_g(from, 0);
 		var terminate = false;
 		var found = false;
 		var last = from;
@@ -149,16 +154,28 @@
 					uval = g(u);
 					found = true;
 					last = u;
-					console.log("!");
+					// console.log("!");
 				}
 			} // neighbours
 			
 		}
-		if(found){
-			var _path = resolvePatch();
-			for(var n of _path)
-				grid.cells[n.y][n.x].add( [-6,0],'"." font Consolas 20 center white', {}, true);
-		}
+		
+		
+		set_g(from, 0);
+		// for (var i = 1; i < res+1; i++)
+		// for (var j = 1; j < res+1; j++){
+			// grid.cells[i-1][j-1].add( [6,0],'"'+visited[i][j]+'" font Consolas 10 center gray', {}, true);
+			// grid.cells[i-1][j-1].add( [6,16],'"'+(i-1)+" "+(j-1)+'" font Consolas 8 center gray', {}, true);
+		// }
+			
+		app.draw();
+		
+		var _path = resolvePatch();
+		// if(found){
+			// for(var n of _path)
+				// grid.cells[n.x][n.y].add( [0,0],'"." font Consolas 20 center red', {}, true);
+			
+		// }
 		return {bool: found, path: _path };
 	};
 
@@ -175,16 +192,119 @@
 				var number = iio.randomInt(1, ballLevel);
 				spawned++;
 				ballCount++;
-				graph[x+1][y+1] = -2;
+				graph[y+1][x+1] = -2;
 				grid.cells[y][x].Ball = number;
-				grid.cells[y][x].add( [0,6],'"'+number+'" font Consolas 20 center white', {}, true);
+				grid.cells[y][x].add( [0,6],'"'+number+'" font Consolas 25 center '+colors[number], {}, true);
 			}
 		}
 		app.draw();
 	};
-	spawn(15);
+	spawn(7);
 	
-	
+	function testBalls(cell){
+		var value = cell.Ball;
+		var x = cell.c;
+		var y = cell.r;
+		
+		var horizontalCount = 0;
+		var verticalCount = 0;
+		var inclinedCount1 = 0;
+		var inclinedCount2 = 0;
+		
+		for(var i=0; i<res; i++){
+			if(x+i<res && grid.cells[x+i][y].Ball == value)
+				horizontalCount++;
+			if(x-i>=0 && grid.cells[x-i][y].Ball == value)
+				horizontalCount++;
+			
+			if(y+i<res && grid.cells[x][y+i].Ball == value)
+				verticalCount++;
+			if(y-i>=0 && grid.cells[x][y-i].Ball == value)
+				verticalCount++;
+			
+			if(y+i<res && x-i>=0 && grid.cells[x-i][y+i].Ball == value)
+				inclinedCount1++;
+			if(y-i>=0 && x+i<res && grid.cells[x+i][y-i].Ball == value)
+				inclinedCount1++;
+			
+			if(y+i<res && x+i<res && grid.cells[x+i][y+i].Ball == value)
+				inclinedCount2++;
+			if(y-i>=0 && x-i>=0 && grid.cells[x-i][y-i].Ball == value)
+				inclinedCount2++;
+		}
+		if(horizontalCount >= 5)
+			points += horizontalCount;
+		if(verticalCount >= 5)
+			points += verticalCount;
+		if(inclinedCount1 >= 5)
+			points += inclinedCount1;
+		if(inclinedCount2 >= 5)
+			points += inclinedCount2;
+
+		console.log(points);
+			
+		if(horizontalCount >= 5){
+			for(var i=0; i<res; i++){
+				if(x+i<res && grid.cells[x+i][y].Ball == value){
+					grid.cells[x+i][y].Ball = 0;
+					grid.cells[x+i][y].rmv(0);
+					graph[x+1+i][y+1] = 0;
+				}
+				if(x-i>=0 && grid.cells[x-i][y].Ball == value){
+					grid.cells[x-i][y].Ball = 0;
+					grid.cells[x-i][y].rmv(0);
+					graph[x+1-i][y+1] = 0;
+				}
+			}
+		}
+		if(verticalCount >= 5){
+			for(var i=0; i<res; i++){
+				if(y+i<res && grid.cells[x][y+i].Ball == value){
+					grid.cells[x][y+i].Ball = 0;
+					grid.cells[x][y+i].rmv(0);
+					graph[x+1][y+1+i] = 0;
+				}
+				if(y-i>=0 && grid.cells[x][y-i].Ball == value){
+					grid.cells[x][y-i].Ball = 0;
+					grid.cells[x][y-i].rmv(0);
+					graph[x+1][y+1-i] = 0;
+				}
+			}
+		}
+		if(inclinedCount1 >= 5){
+			for(var i=0; i<res; i++){
+				if(y+i<res && x-i>=0 && grid.cells[x-i][y+i].Ball == value){
+					grid.cells[x-i][y+i].Ball = 0;
+					grid.cells[x-i][y+i].rmv(0);
+					graph[x+1-i][y+1+i] = 0;
+				}
+				if(y-i>=0 && x+i<res && grid.cells[x+i][y-i].Ball == value){
+					grid.cells[x+i][y-i].Ball = 0;
+					grid.cells[x+i][y-i].rmv(0);
+					graph[x+1+i][y+1-i] = 0;
+				}
+			}
+		}
+		if(inclinedCount2 >= 5){
+			for(var i=0; i<res; i++){
+				if(y+i<res && x+i<res && grid.cells[x+i][y+i].Ball == value){
+					grid.cells[x+i][y+i].Ball = 0;
+					grid.cells[x+i][y+i].rmv(0);
+					graph[x+1+i][y+1+i] = 0;
+				}
+				if(y-i>=0 && x-i>=0 && grid.cells[x-i][y-i].Ball == value){
+					grid.cells[x-i][y-i].Ball = 0;
+					grid.cells[x-i][y-i].rmv(0);
+					graph[x+1-i][y+1-i] = 0;
+				}
+			}
+		}
+		
+		
+		
+		
+		
+	}
 	app.draw();
 
 	app.canvas.oncontextmenu=function(){ return false };
@@ -194,13 +314,14 @@
 		if(event.button==0) {
 			if(selectedCell != 'undefined' && cell != selectedCell && cell.Ball == 0){
 				cell.Ball = selectedCell.Ball;
-				cell.add( [0,6],'"'+cell.Ball+'" font Consolas 20 center white', {}, true);
+				cell.add( [0,6],'"'+cell.Ball+'" font Consolas 25 center '+colors[cell.Ball], {}, true);
 				if(AStar({x:selectedCell.c, y:selectedCell.r, z:0}, {x:cell.c, y:cell.r, z:0}).bool){
 					selectedCell.rmv(0);
 					selectedCell.Ball = 0;
 					graph[selectedCell.c+1][selectedCell.r+1] = 0;
 					graph[cell.c+1][cell.r+1] = -2;
 					selectedCell = 'undefined';
+					testBalls(cell);
 					app.draw();
 					spawn(3);
 				}
